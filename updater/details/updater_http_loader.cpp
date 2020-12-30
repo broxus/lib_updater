9a408c832633d5ca248a3a4e2a845c17755d146f
Rexagon <reide740@gmail.com>
2020-12-30 20:43:26 +0300

    Fix apple clang warnings

--- updater/details/updater_http_loader.cpp
+++ updater/details/updater_http_loader.cpp
@@ -87,7 +87,7 @@ void HttpLoaderActor::sendRequest() {
 
 void HttpLoaderActor::gotMetaData() {
 	const auto pairs = _reply->rawHeaderPairs();
-	for (const auto pair : pairs) {
+	for (const auto &pair : pairs) {
 		if (QString::fromUtf8(pair.first).toLower() == "content-range") {
 			const auto m = QRegularExpression("/(\\d+)([^\\d]|$)").match(QString::fromUtf8(pair.second));
 			if (m.hasMatch()) {
